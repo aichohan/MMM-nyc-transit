d606d5528feae7f9c638cb59b799b3922cec6181
Adeel Chohan <hello@adeelchohan.com>
2026-01-28 16:45:10 -0500

    Fix Hudson Yards station not displaying departures
    Hudson Yards (station 471) trains were not appearing due to a nested loop bug
    in the departure processing logic. The code was processing northbound trains
    correctly but then re-processing all station data again within a nested loop,
    causing duplicate entries and socket notification conflicts.
    
    Changes made:
    - Remove duplicate nested processing loop in node_helper.js (lines 94-145)
    - Fix socket notifications being sent multiple times from within loops
    - Clean up departure processing flow to handle all stations consistently
    
    This resolves the issue where Hudson Yards 7 train departures would not
    display in the module while other stations like Penn Station worked correctly.

--- node_helper.js
+++ node_helper.js
@@ -72,7 +72,7 @@ module.exports = NodeHelper.create({
               }
             })
 
-            // Nothbound Departures
+            // Northbound Departures
             line.departures.N.forEach((i) => {
               for (var key in mtaStationIds) {
                 if (i.destinationStationId === mtaStationIds[key]['Station ID']) {
@@ -89,76 +89,6 @@ module.exports = NodeHelper.create({
                       ? stationIds['606'].name
                       : stationIds[i.destinationStationId].name,
                   walkingTime: walkingTime[n],
-                })
-              }
-              responses.forEach((response, n) => {
-                response.lines.forEach((line) => {
-                  // Southbound Departures
-                  line.departures.S.forEach((i) => {
-                    for (var key in mtaStationIds) {
-                      if (i.destinationStationId === mtaStationIds[key]['Station ID']) {
-                        i.destinationStationId = mtaStationIds[key]['Complex ID']
-                      }
-                    }
-
-                    if (i.destinationStationId !== undefined && dirDownTown[n]) {
-                      downTown.push({
-                        routeId: i.routeId,
-                        time: this.getDate(
-                          i.time,
-                          walkingTime[n]
-                        ),
-                        destination:
-                          i.destinationStationId ==='281'
-                            ? stationIds['606'].name
-                            : stationIds[
-                              i.destinationStationId].name,
-                        walkingTime: walkingTime[n],
-                      })
-                    }
-                  })
-
-                  // Nothbound Departures
-                  line.departures.N.forEach((i) => {
-                    for (var key in mtaStationIds) {
-                      if (i.destinationStationId ===mtaStationIds[key]['Station ID']) {
-                        i.destinationStationId = mtaStationIds[key]['Complex ID']
-                      }
-                    }
-
-                    if (i.destinationStationId !== undefined && dirUpTown[n]) {
-                      upTown.push({
-                        routeId: i.routeId,
-                        time: this.getDate(
-                          i.time,
-                          walkingTime[n]
-                        ),
-                        destination:
-                          i.destinationStationId ==='281'
-                            ? stationIds['606'].name
-                            : stationIds[i.destinationStationId].name,
-                        walkingTime: walkingTime[n],
-                      })
-                    }
-                  })
-                })
-              })
-
-              if (isList) {
-                self.sendSocketNotification('TRAIN_TABLE', {
-                  stations: stations,
-                  data: [
-                    { downTown: downTown.filter((train) => train.time > 0),},
-                    { upTown: upTown.filter((train) => train.time > 0),},
-                  ]
-                })
-              } else {
-                self.sendSocketNotification('TRAIN_TABLE', {
-                  stations: stations,
-                  data: [
-                    { downTown: downTown.filter((train) => train.time > 0).slice(0, 3),},
-                    { upTown: upTown.filter((train) => train.time > 0).slice(0, 3),},
-                  ]
                 })
               }
             })
